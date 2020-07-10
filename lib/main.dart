36db4405ba631304f5161a1b7b2f99baff7eceb4
RishabhS05 <rishabhshrivastava.27@gmail.com>
2020-07-11 01:41:08 +0530

    Buttons -> IconButton, RaisedButton, FlatButton

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,65 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.red[600]),
       body: Center(
-          child: Text(
-        "hello I am center Wiget",
-        style: TextStyle(
-            fontFamily: 'Roboto', fontSize: 30.0, fontStyle: FontStyle.italic),
-      )),
+          /*
+    * Flat Button
+    */
+//          child : FlatButton(
+//        onPressed: () {},
+//        child: Text(
+//          "Click here",
+//          style: TextStyle(
+//              fontFamily: 'Roboto',
+//              fontSize: 30.0,
+//              color: Colors.white,
+//              fontStyle: FontStyle.italic),
+//        ),
+//            color: Colors.blue, )),
+
+/* RaisedButton*/
+//          child: RaisedButton(
+//        onPressed: () {},
+//        child: Text(
+//          "Click here",
+//          style: TextStyle(
+//              fontFamily: 'Roboto',
+//              fontSize: 30.0,
+//              color:Colors.white,
+//              fontStyle: FontStyle.italic),
+//        ),
+//        color: Colors.blue,
+//      )
+          /*Raisedbutton.Icon()*/
+//        child: RaisedButton.icon(
+//          onPressed: () {
+//            print("hello");
+//          },
+//          icon: Icon(
+//            Icons.access_alarms,
+//            color: Colors.white,
+//          ),
+//          textColor: Colors.white,
+//          label: Text("Set Alarm"),
+//          color: Colors.amber,
+//        ),
+          /*IconBotton*/
+          child: IconButton(
+              onPressed: () {
+                print('hello bhai saahab');
+              },
+              icon: Icon(
+                Icons.access_time,
+                color: Colors.blue,
+                size: 60,
+              ))),
       floatingActionButton: FloatingActionButton(
-          child: Text("+"), backgroundColor: Colors.red[600], onPressed: () {}),
+          child: Icon(
+            Icons.access_alarm,
+            color: Colors.blue,
+            size: 60,
+          ),
+          backgroundColor: Colors.transparent,
+          onPressed: () {}),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
