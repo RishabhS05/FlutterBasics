1324b8f3b1e5bcb4fe5f2f461ff55835e30e581a
RishabhS05 <rishabhshrivastava.27@gmail.com>
2020-07-10 19:34:36 +0530

    Working with StatlessWiget and Auto reload.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-        title: Text("Wigets"),
-        centerTitle: true,
-        backgroundColor: Colors.red[600]),
-    body: Center(
-      child: Text("hello I am center Wiget"),
-    ),
-    floatingActionButton: FloatingActionButton(
-        child: Text("+"), backgroundColor: Colors.red[600],
-      onPressed: (){},
-    ),
-       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-  )));
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Wigets"),
+          centerTitle: true,
+          backgroundColor: Colors.red[600]),
+      body: Center(
+        child: Text("hello I am center Wiget"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("+"),
+        backgroundColor: Colors.red[600],
+        onPressed: () {}
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
 }
