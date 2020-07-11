01e1304ec97d2cf2c95fb50effadbb2c8726d459
RishabhS05 <rishabhshrivastava.27@gmail.com>
2020-07-11 16:26:06 +0530

    Listing of data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home()));
+  runApp(MaterialApp(home: HomeListing()));
 }
 
-class Home extends StatelessWidget {
+class HomeListing extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<HomeListing> {
+  List<String>list = ['L1','L2','L3'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-          title: Text("Wigets"),
+          title: Text("Listing Dynamic Data"),
           centerTitle: true,
-          backgroundColor: Colors.red[600]),
-      body: Center(
-          child: Text(
-        "hello I am center Wiget",
-        style: TextStyle(
-            fontFamily: 'Roboto', fontSize: 30.0, fontStyle: FontStyle.italic),
-      )),
+          backgroundColor: Colors.grey[600]),
+      body: Column(
+        children: list.map((l){
+          return Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(l),
+          );
+        }).toList(),
+      ),
       floatingActionButton: FloatingActionButton(
           child: Text("+"), backgroundColor: Colors.red[600], onPressed: () {}),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
