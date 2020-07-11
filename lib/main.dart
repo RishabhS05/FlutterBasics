8f29faaa7eb1190a3c36016b7e815c544e5de9dd
RishabhS05 <rishabhshrivastava.27@gmail.com>
2020-07-11 16:09:25 +0530

    Using statefull wiget make text value increase on pressing FAB.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home()));
+  runApp(MaterialApp(home: HomeStateFullWidget()));
 }
 
 class Home extends StatelessWidget {
@@ -21,6 +21,41 @@ class Home extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
           child: Text("+"), backgroundColor: Colors.red[600], onPressed: () {}),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class HomeStateFullWidget extends StatefulWidget {
+  @override
+  _HomeStateFullWidgetState createState() => _HomeStateFullWidgetState();
+}
+
+class _HomeStateFullWidgetState extends State<HomeStateFullWidget> {
+  int x = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "StateFull widget",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: Center(
+          child: Text('$x'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            x++;
+          });
+        },
+        child: Icon(Icons.add, color: Colors.white),
+      ),
     );
   }
 }
