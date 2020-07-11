8a670535c26241358dec5d8a1c223c15685fc29e
RishabhS05 <rishabhshrivastava.27@gmail.com>
2020-07-11 16:50:33 +0530

    Custom Object list.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'Person.dart';
+
 void main() {
-  runApp(MaterialApp(home: HomeListing()));
+  runApp(MaterialApp(home: CustomList()));
 }
 
 class HomeListing extends StatefulWidget {
@@ -10,7 +12,8 @@ class HomeListing extends StatefulWidget {
 }
 
 class _HomeState extends State<HomeListing> {
-  List<String>list = ['L1','L2','L3'];
+  List<String> list = ['L1', 'L2', 'L3'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +23,46 @@ class _HomeState extends State<HomeListing> {
           centerTitle: true,
           backgroundColor: Colors.grey[600]),
       body: Column(
-        children: list.map((l){
+        children: list.map((l) {
           return Padding(
             padding: const EdgeInsets.all(20),
             child: Text(l),
+          );
+        }).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Text("+"), backgroundColor: Colors.red[600], onPressed: () {}),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class CustomList extends StatefulWidget {
+  @override
+  _CustomListState createState() => _CustomListState();
+}
+
+class _CustomListState extends State<CustomList> {
+  List<Person> personList = [
+    Person(name: 'Rishabh', age: 24),
+    Person(name: 'Anuj', age: 22),
+    Person(name: 'Harsh', age: 34),
+    Person(name: 'Kush', age: 23)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+          title: Text("Listing Dynamic Data"),
+          centerTitle: true,
+          backgroundColor: Colors.grey[600]),
+      body: Column(
+        children: personList.map((person) {
+          return Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text('${person.name} - ${person.age}',style: TextStyle(fontSize: 20),),
           );
         }).toList(),
       ),
