df1d63bc040728db787203037ee43da56045f3e2
RishabhS05 <rishabhshrivastava.27@gmail.com>
2020-07-30 22:46:09 +0530

    CachedNetworkImageProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,14 +13,48 @@ class Home extends StatelessWidget {
           title: Text("Wigets"),
           centerTitle: true,
           backgroundColor: Colors.red[600]),
-      body: Center(
-          child: Image.network(
-              'https://homepages.cae.wisc.edu/~ece533/images/airplane.png')
-//          child: Image(
-//        image: NetworkImage(
-//            'https://homepages.cae.wisc.edu/~ece533/images/airplane.png'),
-//      )
+      body: ListView(
+        children: <Widget>[
+          Container(
+            width:200,
+            height: 400,
+            child: CachedNetworkImage(
+              imageUrl:
+                  "https://homepages.cae.wisc.edu/~ece533/images/airplane.png",
+              imageBuilder: (context, imageProvider) => Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
           ),
+          Container(
+            width: 200,
+            height: 400,
+            child: CachedNetworkImage(
+              imageUrl: "https://homepages.cae.wisc.edu/~ece533/images/boat.png",
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+          ),
+          Container(
+            width: 200,
+            height: 400,
+            child: CachedNetworkImage(
+              imageUrl: "https://homepages.cae.wisc.edu/~ece533/images/arctichare.png",
+              progressIndicatorBuilder: (context, url, downloadProgress) =>
+                  CircularProgressIndicator(value: downloadProgress.progress),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+          ),
+          Container(child: Image(image: CachedNetworkImageProvider("https://homepages.cae.wisc.edu/~ece533/images/cat.png")),)
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
           child: Center(
             child: Image.asset('assets/profile.png'),
