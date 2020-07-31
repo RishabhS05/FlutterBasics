d4adf218776282a689785b65cb784ca83e5d26b9
RishabhS05 <rishabhshrivastava.27@gmail.com>
2020-07-31 09:12:53 +0530

    Sharedpreference demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,81 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home()));
+  runApp(MyApp());
 }
 
-class Home extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SharedPreferences Demo',
+      home: SharedPreferencesDemo(),
+    );
+  }
+}
+
+class SharedPreferencesDemo extends StatefulWidget {
+  SharedPreferencesDemo({Key key}) : super(key: key);
+
+  @override
+  SharedPreferencesDemoState createState() => SharedPreferencesDemoState();
+}
+
+class SharedPreferencesDemoState extends State<SharedPreferencesDemo> {
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  Future<int> _counter;
+
+  Future<void> _incrementCounter() async {
+    final SharedPreferences prefs = await _prefs;
+    final int counter = (prefs.getInt('counter') ?? 0) + 1;
+
+    setState(() {
+      _counter = prefs.setInt("counter", counter).then((bool success) {
+        return counter;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _counter = _prefs.then((SharedPreferences prefs) {
+      return (prefs.getInt('counter') ?? 0);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("Wigets"),
-          centerTitle: true,
-          backgroundColor: Colors.red[600]),
+        title: const Text("SharedPreferences Demo"),
+      ),
       body: Center(
-          child: Text(
-        "hello I am center Wiget",
-        style: TextStyle(
-            fontFamily: 'Roboto', fontSize: 30.0, fontStyle: FontStyle.italic),
-      )),
+          child: FutureBuilder<int>(
+              future: _counter,
+              builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return const CircularProgressIndicator();
+                  default:
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return Text(
+                        'Button tapped ${snapshot.data} time${snapshot.data == 1 ? '' : 's'}.\n\n'
+                            'This should persist across restarts.',
+                      );
+                    }
+                }
+              })),
       floatingActionButton: FloatingActionButton(
-          child: Text("+"), backgroundColor: Colors.red[600], onPressed: () {}),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
