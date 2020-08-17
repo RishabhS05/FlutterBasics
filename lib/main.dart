56f7e822d968ab224743aa47862b6ae9d3d840df
RishabhS05 <rishabhshrivastava.27@gmail.com>
2020-08-17 18:20:57 +0530

    Added the mpeg ip tv.

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,63 @@
-import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_mjpeg/flutter_mjpeg.dart';
 
-void main() => runApp(VideoApp());
+void main() => runApp(MyApp());
 
-class VideoApp extends StatefulWidget {
-  @override
-  _VideoAppState createState() => _VideoAppState();
-}
-
-class _VideoAppState extends State<VideoApp> {
-  VideoPlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.network(
-        'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_30MB.mp4')
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
-      });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Video Demo',
-      home: Scaffold(
-        body: Center(
-          child: _controller.value.initialized
-              ? AspectRatio(
-            aspectRatio: _controller.value.aspectRatio,
-            child: VideoPlayer(_controller),
-          )
-              : Container(),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _controller.value.isPlaying
-                  ? _controller.pause()
-                  : _controller.play();
-            });
-          },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-          ),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(),
     );
   }
+}
 
+class MyHomePage extends HookWidget {
   @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
+  Widget build(BuildContext context) {
+    final isRunning = useState(true);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Mjpeg(
+                isLive: isRunning.value,
+                stream:'http://25.134.76.210:8080/video'
+ //'http://192.168.1.37:8081',
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  isRunning.value = !isRunning.value;
+                },
+                child: Text('Toggle'),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) =>
+                          Scaffold(
+                            appBar: AppBar(),
+                          )));
+                },
+                child: Text('Push new route'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
