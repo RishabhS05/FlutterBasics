ee428e137c174c142a4c5696a96957fb6cd88d79
RishabhS05 <rishabhshrivastava.27@gmail.com>
2020-08-17 16:17:19 +0530

    Video Player

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,59 @@
+import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(home: Home()));
+void main() => runApp(VideoApp());
+
+class VideoApp extends StatefulWidget {
+  @override
+  _VideoAppState createState() => _VideoAppState();
 }
 
-class Home extends StatelessWidget {
+class _VideoAppState extends State<VideoApp> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_30MB.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text("Wigets"),
-          centerTitle: true,
-          backgroundColor: Colors.red[600]),
-      body: Center(
-          child: Text(
-        "hello I am center Wiget",
-        style: TextStyle(
-            fontFamily: 'Roboto', fontSize: 30.0, fontStyle: FontStyle.italic),
-      )),
-      floatingActionButton: FloatingActionButton(
-          child: Text("+"), backgroundColor: Colors.red[600], onPressed: () {}),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    return MaterialApp(
+      title: 'Video Demo',
+      home: Scaffold(
+        body: Center(
+          child: _controller.value.initialized
+              ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          )
+              : Container(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _controller.value.isPlaying
+                  ? _controller.pause()
+                  : _controller.play();
+            });
+          },
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
