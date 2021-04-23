17811b164872d914502d2680ef0086b898704bf5
RishabhS05 <rishabhshrivastava.27@gmail.com>
2021-04-23 22:14:27 +0530

    Add Chrome and saffari brouser.

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'chrome_safari_web_view.dart';
+import 'new_plugin_web_view_file.dart';
+
 void main() {
-  runApp(MaterialApp(home: Home()));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(home: ChromeSafariBrowserExampleScreen()));
 }
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text("Wigets"),
-          centerTitle: true,
-          backgroundColor: Colors.red[600]),
+      appBar: AppBar(title: Text("Wigets"), centerTitle: true, backgroundColor: Colors.red[600]),
       body: Center(
           child: Text(
         "hello I am center Wiget",
-        style: TextStyle(
-            fontFamily: 'Roboto', fontSize: 30.0, fontStyle: FontStyle.italic),
+        style: TextStyle(fontFamily: 'Roboto', fontSize: 30.0, fontStyle: FontStyle.italic),
       )),
-      floatingActionButton: FloatingActionButton(
-          child: Text("+"), backgroundColor: Colors.red[600], onPressed: () {}),
+      floatingActionButton: FloatingActionButton(child: Text("+"), backgroundColor: Colors.red[600], onPressed: () {}),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
